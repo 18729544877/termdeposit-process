08da94cc360cdc4e2a7f473fa19db830ab81313a
Administrator <Administrator@PC-20190717ZSCD>
2019-08-07 00:57:11 +0800

    init

---
+++ src/main/java/com/simnectzbank/lbs/processlayer/termdepositprocess/TermdepositProcessApplication.java
@@ -0,0 +1,13 @@
+package com.simnectzbank.lbs.processlayer.termdepositprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermdepositProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermdepositProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simnectzbank/lbs/processlayer/termdepositprocess/TermdepositProcessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simnectzbank.lbs.processlayer.termdepositprocess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TermdepositProcessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
